845d954b998dad0876e86fdb3a87c80b9d785291
scallop2003 <tuannghia200603@gmail.com>
2023-08-30 10:23:42 +0700

    Add graphic

--- main.py
+++ main.py
@@ -1,18 +1,26 @@
 import numpy as np
-
+import matplotlib.pyplot as plt
 def Loss(theta):
     return theta**2 - theta * 4 + 10
+
+theta = np.arange(-4, 8, 0.1)
+loss = Loss(theta)
+plt.plot(theta, loss)
+# plt.show()
 
 def DerLoss(theta):
     return 2 * theta - 4
 
-theta = -5
+theta = -4
 alpha = 0.01
 es = 0.0001
 
 while True:
     theta = theta - alpha * DerLoss(theta)
-    if (abs(DerLoss(theta)) < es):
+    plt.plot(theta, Loss(theta), 'ro')
+    plt.pause(0.3)
+    if abs(DerLoss(theta)) < es:
         break
+plt.show()
 print(Loss(theta))
 print(theta)
